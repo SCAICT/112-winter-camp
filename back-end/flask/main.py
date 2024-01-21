3607ee533bd636ebacc53477dc1e76d7eacfd112
Edit-Mr <elvismao.070512@gmail.com>
2024-01-21 17:50:03 +0800

    行前通知書

--- back-end/flask/main.py
+++ back-end/flask/main.py
@@ -395,5 +395,5 @@ def update():
         return redirect('/admin')
     return updateDCStudentCount()
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=80)
+
+app.run(debug=True, host="0.0.0.0", port=80,use_reloader=False)
